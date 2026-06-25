8c1ec97a4006adea20c5e51942db84e2b9c74ef5
Adi M. Yasir <adimuhammadyasir93@gmail.com>
2026-06-26 01:41:16 +0700

    fix: dynamic port

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,26 @@ import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 import { authPlugin } from "./modules/auth/auth.js";
 
-const app = new Elysia()
-  .use(
-    cors({
-      origin: process.env.FRONTEND_URL ?? "http://localhost:5173",
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      credentials: true,
-      allowedHeaders: ["Content-Type", "Authorization"],
-    })
-  )
-  .use(authPlugin)
-  .get("/health", () => ({
-    status: "ok",
-  }))
-  .listen(3000);
+const main = () => {
+  const app = new Elysia()
+    .use(
+      cors({
+        origin: process.env.FRONTEND_URL ?? "http://localhost:5173",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+        allowedHeaders: ["Content-Type", "Authorization"],
+      })
+    )
+    .use(authPlugin);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+  app.get("/health", () => ({
+    status: "ok",
+  }));
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}...`);
+  });
+};
+
+main();
